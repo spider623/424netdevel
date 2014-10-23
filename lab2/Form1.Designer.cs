e7e0d7009c8baf682b0d965ab66e6fe233125961
Simos Katsiaris <sim.osk@live.com>
2014-10-24 00:00:16 +0300

    First commit
    all working

---
+++ lab2/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAsync = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 258);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnAsync
+            // 
+            this.btnAsync.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAsync.Location = new System.Drawing.Point(12, 276);
+            this.btnAsync.Name = "btnAsync";
+            this.btnAsync.Size = new System.Drawing.Size(75, 23);
+            this.btnAsync.TabIndex = 1;
+            this.btnAsync.Text = "Async";
+            this.btnAsync.UseVisualStyleBackColor = true;
+            this.btnAsync.Click += new System.EventHandler(this.doRead);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Location = new System.Drawing.Point(197, 276);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(284, 311);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnAsync);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Async File Read";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAsync;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ lab2/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        FileStream fs;
+        byte[] fileContends;
+        
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+
+        public void doRead(object sender, EventArgs e)
+        { 
+
+            int count = 1;
+            while (count == 1)
+            {
+                openFileDialog1.ShowDialog();
+                try
+                {
+
+                   using (fs = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, false))
+                    {
+                        count = 0;
+                        // Read the source file into a byte array. 
+                        fileContends = new byte[fs.Length];
+                        int numBytesToRead = (int)fs.Length;
+                        int numBytesRead = 0;
+                        while (numBytesToRead > 0)
+                        {
+                            // Read may return anything from 0 to numBytesToRead. 
+                            int n = fs.Read(fileContends, numBytesRead, numBytesToRead);
+
+                            // Break when the end of the file is reached. 
+                            if (n == 0)
+                                break;
+
+                            numBytesRead += n;
+                            numBytesToRead -= n;
+                        }
+                        numBytesToRead = fileContends.Length; 
+                        string text = Encoding.UTF8.GetString(fileContends);
+                        textBox1.AppendText(text);
+                        
+                    }
+                }
+                catch (FileNotFoundException ioEx)
+                {
+
+                    textBox1.AppendText(ioEx.Message);
+                    count = 0;
+                }
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+              
+
+        }
+        private void btnAsync_clic() {
+
+            doRead();
+        }
+
+        private void doRead()
+        {
+        
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    
+}
+}
